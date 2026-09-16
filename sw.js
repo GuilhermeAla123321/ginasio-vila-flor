e0c83af5e9016b927415859fd7cb38fd8caa40fc
Guilherme <51718@alunos.upt.pt>
2026-09-17 00:24:45 +0100

    1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vf-ginasio-v10';
+const CACHE_NAME = 'vf-ginasio-v11';
 const APP_SHELL = [
   './', './index.html', './manifest.json',
   './css/style.css', './css/responsive.css',
